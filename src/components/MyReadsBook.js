423dc626a2598f790ff297d71cd3067441b02c97
Jeff Gan <developer.jeff.gan@gmail.com>
2018-03-10 12:02:31 +0800

    Option default value to None if shelf is not present

--- src/components/MyReadsBook.js
+++ src/components/MyReadsBook.js
@@ -1,43 +1,26 @@
-import React, {Component} from 'react'
-import PropTypes from 'prop-types'
+import React from 'react'
+import MyReadsShelves from '../enums/MyReadsShelves';
 
-class MyReadsBook extends Component {
-    static propTypes = {
-        title: PropTypes.string
-        , authors: PropTypes.array
-        , shelf: PropTypes.oneOf(['currentlyReading', 'wantToRead', 'read'])
-        , imageUrl: PropTypes.string.isRequired
-    }
+const MyReadsBook = (props) => (
 
-    state = {
-        
-    }
+    <div className="book">
+    <div className="book-top">
+        <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${props.book.imageLinks.smallThumbnail})` }}></div>
+        <div className="book-shelf-changer">
 
-    onShelfChanged = (event) => {
-
-    }
-
-    render() {
-        return (
-            <div className="book">
-                <div className="book-top">
-                <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${this.props.imageUrl})` }}></div>
-                <div className="book-shelf-changer">
-                    <select onChange={this.onShelfChanged}>
-                    <option value="nothing" disabled>Move to...</option>
-                    <option value="currentlyReading">Currently Reading</option>
-                    <option value="wantToRead">Want to Read</option>
-                    <option value="read">Read</option>
-                    <option value="none">None</option>
-                    </select>
-                </div>
-                </div>
-                <div className="book-title">this.props.title</div>
-                <div className="book-authors">this.props.author</div>
-            </div>
-
-        )
-    }
-}
+        <select onChange={(event) => props.onShelfChanged(props.book, event)} value={"shelf" in props.book ? props.book.shelf : MyReadsShelves.NONE.value }>
+            <option value="nothing" disabled>Move to...</option>
+            {MyReadsShelves.enumValues.map(({value, description}) => (
+                <option key={value} value={value}>{description}</option>
+                ))
+            }
+            
+        </select>
+        </div>
+    </div>
+    <div className="book-title">{props.book.title}</div>
+    <div className="book-authors">{props.book.authors}</div>
+    </div>
+)
 
 export default MyReadsBook
